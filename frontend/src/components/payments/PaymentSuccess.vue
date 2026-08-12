569038aeef8f3d9348e79fbdeba8fc26af90ef84
{-Ovidiu Sandu-} <{-ovy79ro@yahoo.com-}>
2026-08-12 17:47:17 +0300

    wip

---
+++ frontend/src/components/payments/PaymentSuccess.vue
@@ -0,0 +1,17 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div class="row my-5">
+        <div class="col-md-6 mx-auto">
+            <div class="card">
+                <div class="card-body p-5">
+                    <div class="text-center">
+                        Payment done successfully!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ frontend/src/components/payments/Stripe.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import axios from 'axios';
+    import { useCartStore } from '@/store/useCartStore';
+
+    const store = useCartStore()
+
+    const fetchPaymentUrl = async() => {
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/pay/order', {
+                cartItems: store.getCartItems, success_url: 'http://localhost:5173/#/payment/success'
+            })
+            window.location.href = response.data.url
+        } catch (error) {
+            console.log(error)
+        }
+    }
+</script>
+
+<template>
+    <div class="row mt-5">
+        <div class="col-md-12">
+            <button class="btn btn-dark" type="button" @click="fetchPaymentUrl">
+                Pay now
+            </button>
+        </div>
+    </div>
+</template>
